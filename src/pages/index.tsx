35c63dfb7d3e65a584d16ee7eb65a65ec8e30454
hbsnow <yuki.4uing@gmail.com>
2021-07-25 05:35:46 +0900

    fix: コンポーネントの置き換え

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,29 @@ import React from "react";
 import { NextPage } from "next";
 import { classnames } from "tailwindcss-classnames";
 
-import { Button } from "../components/Button";
+import { DraggableTable } from "../components/DraggableTable";
+
+const data = {
+  rows: [
+    { id: "row-1", value: "row 1" },
+    { id: "row-2", value: "row 2" },
+    { id: "row-3", value: "row 3" },
+    { id: "row-4", value: "row 4" },
+    { id: "row-5", value: "row 5" },
+  ],
+  columns: [
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+    ["1", "2", "3", "4", "5"],
+  ],
+};
 
 const Page: NextPage = () => {
   return (
-    <main className={classnames("container")}>
-      <Button>button</Button>
+    <main className={classnames("p-4")}>
+      <DraggableTable {...data} />
     </main>
   );
 };
